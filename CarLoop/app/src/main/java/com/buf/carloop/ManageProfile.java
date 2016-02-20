6a3e2b1d21594b8d2cfe38f1e209fed7a3374ea6
JohnLeeBUAA <lizijin123@sina.com>
2016-02-20 02:24:58 -0500

    change user.u_avatar from varchar to blob, finish user module

--- CarLoop/app/src/main/java/com/buf/carloop/ManageProfile.java
+++ CarLoop/app/src/main/java/com/buf/carloop/ManageProfile.java
@@ -1,14 +1,38 @@
 package com.buf.carloop;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class ManageProfile extends Footer {
+
+    private ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +41,38 @@ public class ManageProfile extends Footer {
         ViewGroup.inflate(this, R.layout.activity_manage_profile, vg);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        avatar = (ImageView) findViewById(R.id.avatar_manage_profile);
     }
 
+    public void selectImage(View view) {
+        Intent intent;
+        if (Build.VERSION.SDK_INT < 19){
+            intent = new Intent();
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, 123);
+        } else {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            startActivityForResult(intent, 123);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 123) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    Uri selectedImage = data.getData();
+                    InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+                    Bitmap bm = BitmapFactory.decodeStream(imageStream);
+                    avatar.setImageBitmap(bm);
+                } catch (FileNotFoundException e) {
+
+                }
+            }
+        }
+    }
 }
