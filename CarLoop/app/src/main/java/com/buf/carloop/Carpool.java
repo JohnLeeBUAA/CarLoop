b769129cd9344cebfe9ae642f6186369c25408dc
xin <kjinxin@gmail.com>
2016-03-08 14:29:28 -0800

    add the function of create a carpool

--- CarLoop/app/src/main/java/com/buf/carloop/Carpool.java
+++ CarLoop/app/src/main/java/com/buf/carloop/Carpool.java
@@ -186,9 +186,9 @@ public class Carpool {
             int status
     ) {
         String sqlComm = String.format("insert into carpool_created (cc_drivername, cc_depart_lat, cc_depart_lng, cc_depart_loc, " +
-                "cc_desti_lat, cc_desti_lng, cc_desti_loc, cc_date, cc_date_range, cc_time, cc_time_range, cc_maxpassenger, " +
-                "cc_price, cc_passengerconfirmed, cc_passengeraboard, cc_status) values ('%s', %f, %f, '%s', '%f, '%f, '%s', " +
-                "'%s', '%s', '%s', '%s', '%s', %d, %d, %d, %d, %d);", user_name, depart_lat, depart_lng, depart_loc, desti_lat, desti_lng,
+                        "cc_desti_lat, cc_desti_lng, cc_desti_loc, cc_date, cc_date_range, cc_time, cc_time_range, cc_maxpassenger, " +
+                        "cc_price, cc_passengerconfirmed, cc_passengeraboard, cc_status) values ('%s', %f, %f, '%s', %f, %f, '%s', " +
+                        "'%s', '%s', '%s', '%s', %d, %d, %d, %d, %d);", user_name, depart_lat, depart_lng, depart_loc, desti_lat, desti_lng,
                 desti_loc, date, date_range, time, time_range, maxpassenger, price, passengerconfirmed, passengeraboard, status);
         // Sql create user operation
         AsyncSQLLongHaul task = new AsyncSQLLongHaul();
@@ -222,10 +222,10 @@ public class Carpool {
             int maxpassenger,
             int price
     ) {
-        String sqlComm = String.format("update carpool_created set cc_depart_lat=%d, cc_depart_lng=%d, cc_depart_loc='%s', " +
-                        "cc_desti_lat=%d, cc_desti_lng=%d, cc_desti_loc='%s', cc_date='%s', cc_date_range='%s', cc_time='%s', " +
-                        "cc_time_range='%s', cc_maxpassenger=%d, cc_price=%d, cc_status=%d where cc_id=%d;",
-                        depart_lat, depart_lng, depart_loc, desti_lat, desti_lng, desti_loc, date, date_range, time, time_range, maxpassenger, price);
+        String sqlComm = String.format("update carpool_created set cc_depart_lat=%f, cc_depart_lng=%f, cc_depart_loc='%s', " +
+                        "cc_desti_lat=%f, cc_desti_lng=%f, cc_desti_loc='%s', cc_date='%s', cc_date_range='%s', cc_time='%s', " +
+                        "cc_time_range='%s', cc_maxpassenger=%d, cc_price=%d where cc_id=%d;",
+                        depart_lat, depart_lng, depart_loc, desti_lat, desti_lng, desti_loc, date, date_range, time, time_range, maxpassenger, price, carpoolid);
         // Sql create user operation
         AsyncSQLLongHaul task = new AsyncSQLLongHaul();
         task.execute(sqlComm);
