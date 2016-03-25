7c845106e1e8d620a9cf1b3872ef3323a66cade4
JohnLeeBUAA <lizijin123@sina.com>
2016-03-25 18:35:05 -0400

    add timestamp

--- CarLoop/app/src/main/java/com/buf/carloop/GlobalVariables.java
+++ CarLoop/app/src/main/java/com/buf/carloop/GlobalVariables.java
@@ -7,4 +7,5 @@ public class GlobalVariables {
     public static String paypal_sdk_id = "AVPJrIoE08c25n1i3IGM6C4Ryxi4z5G0-jXKomA3fODCiVX8WzJqLazT_TQcXz97ySMsJI2-91KUH6DN";
     public static String user_name = "";
     public static int user_identity = -1; //passenger only - 0; driver - 1; driver using as passenger - 2
+    public static String timestamp;
 }
