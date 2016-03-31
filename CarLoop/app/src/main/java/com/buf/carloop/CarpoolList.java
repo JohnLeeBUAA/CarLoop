1f0bb6306af8971e2b9f19dd82a4308c95cf31a9
xin <kjinxin@gmail.com>
2016-03-30 20:15:16 -0700

    repair the bug of calculating the distance

--- CarLoop/app/src/main/java/com/buf/carloop/CarpoolList.java
+++ CarLoop/app/src/main/java/com/buf/carloop/CarpoolList.java
@@ -508,7 +508,8 @@ public class CarpoolList extends Footer {
 
     private void longDistanceFilter() {
         if(list != null && list.size() != 0) {
-            for(int i = 0; i < list.size(); i++) {
+            int size = list.size();
+            for(int i = 0; i < size; i++) {
                 double walk_distance_depart = getWalkDistance(search_depart_lat, search_depart_lng, list.get(i).getDepart_lat(), list.get(i).getDepart_lng());
                 double walk_distance_desti = getWalkDistance(search_desti_lat, search_desti_lng, list.get(i).getDesti_lat(), list.get(i).getDesti_lng());
                 if(walk_distance_depart + walk_distance_desti > 0.5) {
